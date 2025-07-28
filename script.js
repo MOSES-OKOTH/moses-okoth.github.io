c1d3ba3570c8c858118b570313427143a101d9ef
MOSES OKOTH <mosesokoth164@gmail.com>
2025-07-28 18:28:38 +0300

    Added the what I do section

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function removePreloader(){
     let int = setInterval(function(){
         if (preloader) {
             preloader.style.display = 'none';
+            document.title = "Portfolio | Moses Okoth";
             clearInterval(int);
         }
     }, time);
@@ -15,9 +16,9 @@ function removePreloader(){
 
 
 /* Opening and Closing the Hamburger Menu */
-let hamBtn = document.querySelector('.ham-menu');
-let closeHamBtn = document.querySelector('.close-ham-menu');
-let links = document.querySelector('.links');
+let hamBtn = document.querySelector('.mobile-ham-menu');
+let closeHamBtn = document.querySelector('.mobile-close-ham-menu');
+let links = document.querySelector('.mobile-links');
 
 
 hamBtn.addEventListener('click', function(){
@@ -32,4 +33,5 @@ closeHamBtn.addEventListener('click', function(){
 
 links.addEventListener('click', function(e){
     links.style.display = 'none';
-})
+    closeHamBtn.style.display = 'none';
+});
